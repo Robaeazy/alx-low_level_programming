e3ddb35963a81b53d91a0399b3357f6713c93ebe
Robaeazy <robabest0@gmail.com>
2022-10-13 07:14:23 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Description: A C program that prints with put function
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *      */
+
+
+
 int main(void)
-	/**
-	 *
-	 *  * main - Entry point
-	 *
-	 *   *
-	 *
-	 *    * Description: A C program that prints with put function
-	 *
-	 *     *
-	 *
-	 *      * Return: Always 0 (Success)
-	 *
-	 *      */
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
