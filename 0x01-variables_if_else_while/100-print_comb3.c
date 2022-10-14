bc6bd07e003b20f0376fb0bd8eb9f543f9e009de
Robaeazy <robabest0@gmail.com>
2022-10-14 16:21:47 -0700

    100-print-num

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,6 +9,9 @@ int main(void)
 int digit1 = 0, digit2;
 while (digit1 <= 9)
 {
+digit2 = 0;
+while (digit2 <= 9)
+{
 if (digit1 != digit2 && digit1 < digit2)
 {
 putchar(digit1 + 48);
@@ -22,6 +25,7 @@ putchar(' ');
 ++digit2;
 }
 ++digit1;
+}
 putchar('\n');
 return (0);
 }
